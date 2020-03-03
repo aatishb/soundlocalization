7a3d4944001a9d0f8cd7ae3d645a4fd9789ae17b
Aatish Bhatia <aatish@gmail.com>
2020-03-03 12:25:20 -0500

    fix safari bugs

--- itd/sketch.js
+++ itd/sketch.js
@@ -1,7 +1,9 @@
 window.onload = function() {
   const URL = 'doorbellclip.mp3';
-    
+
+  var AudioContext = window.AudioContext || window.webkitAudioContext;
   const context = new AudioContext();
+
   const LButton = document.querySelector('#playL');
   const MButton = document.querySelector('#playM');
   const RButton = document.querySelector('#playR');
@@ -13,20 +15,22 @@ window.onload = function() {
 
   slider.oninput = function() {
     output.innerHTML = this.value/40 + ' ms delay';
-  } 
-    
+  }
+
   let yodelBuffer;
 
   window.fetch(URL)
     .then(response => response.arrayBuffer())
-    .then(arrayBuffer => context.decodeAudioData(arrayBuffer))
+    .then(arrayBuffer => context.decodeAudioData(arrayBuffer,
+          audioBuffer => {yodelBuffer = audioBuffer;},
+          error => console.error(error)))
     .then(audioBuffer => {
       LButton.disabled = false;
       MButton.disabled = false;
       RButton.disabled = false;
       yodelBuffer = audioBuffer;
     });
-    
+
     LButton.onclick = () => play(yodelBuffer, 'left');
     MButton.onclick = () => play(yodelBuffer, 'mid');
     RButton.onclick = () => play(yodelBuffer, 'right');
@@ -34,15 +38,15 @@ window.onload = function() {
   function play(audioBuffer, direction) {
     const source = context.createBufferSource();
     source.buffer = audioBuffer;
-      
+
     //var splitter = context.createChannelSplitter(2);
     //source.connect(splitter);
 
     var leftDelay = context.createDelay();
     var rightDelay = context.createDelay();
-    
+
     var delay = slider.value/40;
-      
+
     if (direction == 'mid') {
         leftDelay.delayTime.value = 0/1000;
         rightDelay.delayTime.value = 0/1000;
@@ -59,13 +63,13 @@ window.onload = function() {
 
     source.connect(leftDelay, 0);
     source.connect(rightDelay, 0);
-      
+
     var merger = context.createChannelMerger(2);
     leftDelay.connect(merger, 0, 0);
     rightDelay.connect(merger, 0, 1);
-      
+
     merger.connect(context.destination);
-      
+
     source.start();
   }
 };
